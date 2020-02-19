cdf3374f9c8d9982a3043cf51c298816877129e4
super1or <38477985+super1or@users.noreply.github.com>
2020-02-19 23:00:52 +0000

    Added localStorage class. Now the the data saves after hard reload

--- appES6.js
+++ appES6.js
@@ -20,7 +20,7 @@ class UI {
         `;
         // Appending to list
         list.appendChild(row);
-    } 
+    }
 
     showAlert(message, className) {
         // Create a div
@@ -58,7 +58,54 @@ class UI {
     }
 }
 
-// Event Lesteners
+// Local storage class
+class Store {
+    static getBooks() {
+        let books;
+        if (localStorage.getItem('books') === null) {
+            books = [];
+        } else {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+        return books;
+    }
+
+    static displayBooks() {
+        const books = Store.getBooks();
+
+        books.forEach(function(book) {
+            const ui = new UI;
+
+            // Add book to UI
+            ui.AddBookToList(book);
+        })
+    }
+
+    static addBook(book) {
+        const books = Store.getBooks();
+        
+        books.push(book);
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    static removeBook(isbn) {
+        const books = Store.getBooks();
+
+        books.forEach(function (book, index) {
+            if (book.isbn === isbn) {
+                books.splice(index, 1);
+            }
+        });
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
+
+// DOM load event
+document.addEventListener('DOMContentLoaded', Store.displayBooks);
+
+// Event Lesteners to add a book
 document.getElementById('book-form').addEventListener('submit', function(e) {
     // Get form values
     const title = document.getElementById('title').value,
@@ -79,6 +126,9 @@ document.getElementById('book-form').addEventListener('submit', function(e) {
         // Add book to list
         ui.AddBookToList(book);
 
+        // Add to local storage
+        Store.addBook(book);
+
         // Show alert
         ui.showAlert('Book successfully added!', 'success');
 
@@ -94,7 +144,11 @@ document.getElementById('book-list').addEventListener('click', function(e) {
     // Instantiate UI
     const ui = new UI();
 
+    // Remove book
     ui.deleteBook(e.target);
+
+    // Remove from local storage
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
     // Show Alert
     ui.showAlert('Book has been removed!', 'success');
